8b6ebe699aa3b8116bcbbd06dd92d7678b3082b5
Ayoub Akritah <aakritah@c2r9p5.1337.ma>
2025-04-09 21:49:37 +0100

    Add

--- parsing/parse.c
+++ parsing/parse.c
@@ -6,29 +6,28 @@
 /*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 02:15:07 by aakritah          #+#    #+#             */
-/*   Updated: 2025/04/09 21:22:35 by aakritah         ###   ########.fr       */
+/*   Updated: 2025/04/09 21:48:42 by aakritah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/main.h"
 #include "../include/parse.h"
 
-void ft_parse(char *str)
+void	ft_parse(char *str)
 {
-    t_token *data;
-    data=NULL;
+	t_token	*data;
+
+	data = NULL;
+	if (ft_tokenize(str, &data) < 0)
+	{
+		printf("\n---------\n");
+		printf("Error");
+		printf("\n---------\n");
+		ft_free_list(&data);
+		return ;
+	}
+
     
-    if(ft_tokenize(str,&data)<0)
-    {
-        printf("\n---------\n");
-        printf("Error");
-        printf("\n---------\n");
-        ft_free_list(&data); 
-        return ;   
-    }
-
-   
-
-    ft_print_list(data);
-    ft_free_list(&data);
+	ft_print_list(data);
+	ft_free_list(&data);
 }
